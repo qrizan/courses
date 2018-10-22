111522263201db782f1bfd150f131bb6352e12fe
qrizan <qrizan@gmail.com>
2018-10-23 00:40:56 +0700

    update config

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 # app/config.py
 
 class Config:
-    SECRET_KEY = '0e4cf9a00d41eec96d7eaf326769cc62'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:q@localhost/courses_db'
+    SECRET_KEY = '********************************'
+    SQLALCHEMY_DATABASE_URI = 'mysql://user:password@localhost/database_name'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'rizan170@gmail.com'
-    MAIL_PASSWORD = 'dua5151tujuh'
+    MAIL_USERNAME = '******@gmail.com'
+    MAIL_PASSWORD = '***************'
